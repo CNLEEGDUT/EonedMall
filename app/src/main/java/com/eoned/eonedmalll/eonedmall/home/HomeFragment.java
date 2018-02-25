1f630e20e7887947c05fc83091edaa93fc0e80e3
LEE <cnleegdut@gmail.com>
2018-02-25 11:15:54 +0800

    201802251115

--- app/src/main/java/com/eoned/eonedmalll/eonedmall/home/HomeFragment.java
+++ app/src/main/java/com/eoned/eonedmalll/eonedmall/home/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.eoned.eonedmalll.eonedmall.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.eoned.eonedmalll.eonedmall.R;
 import com.eoned.eonedmalll.eonedmall.base.BaseFragment;
+import com.eoned.eonedmalll.eonedmall.views.DividerGridItemDecoration;
+import com.eoned.eonedmalll.eonedmall.views.FlyBanner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/2/24.
@@ -15,10 +23,64 @@ import com.eoned.eonedmalll.eonedmall.base.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
         return view;
+    }
+
+
+    /**
+     * 初始化界面
+     */
+    private RecyclerView mRecyclerView;
+
+    private void initView(View view) {
+        if (getContext() == null) {
+            return;
+        }
+        mRecyclerView = view.findViewById(R.id.rv_home);
+        HomeRvAdapter mHomeRvAdapter = new HomeRvAdapter();
+
+        initbanner(mHomeRvAdapter);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        // 设置布局管理一条数据占用几行，如果是头布局则头布局自己占用一行
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int postion) {
+                if (postion == 0) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mRecyclerView.setAdapter(mHomeRvAdapter);
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(getContext()));
+    }
+
+
+    /**
+     * 初始化topbanner
+     */
+    private void initbanner(HomeRvAdapter mHomeRvAdapter) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_home_topbanner, null);
+        FlyBanner topBanner = view.findViewById(R.id.fb_home_top);
+
+        List<String> bigPics = new ArrayList<>();
+        bigPics.add("https://gd2.alicdn.com/imgextra/i2/380101244/TB2HHzZdNmJ.eBjy0FhXXbBdFXa_!!380101244.jpg");
+        bigPics.add("https://gd4.alicdn.com/imgextra/i4/380101244/TB2qUNua4mI.eBjy0FlXXbgkVXa_!!380101244.jpg");
+        bigPics.add("https://gd1.alicdn.com/imgextra/i1/380101244/TB2REFwa9qJ.eBjy1zbXXbx8FXa_!!380101244.jpg");
+        bigPics.add("https://gd4.alicdn.com/imgextra/i4/380101244/TB2Ye4taZeK.eBjSszgXXczFpXa_!!380101244.jpg");
+        bigPics.add("http://ob9thtnhs.bkt.clouddn.com/tuisong/da386d4d7872451ca346ba6e37da17b6.jpg?e=1477544913&token=m2BF8x75sZF4DIfwnxFri5sT51HeuFRmU2Ue0uVf:LWGBH77qhKA_BEcdgUA5u5AebR0=");
+
+        topBanner.setImagesUrl(bigPics);
+
+        mHomeRvAdapter.setHeadView0(view);
     }
 }
---
+++ app/src/main/java/com/eoned/eonedmalll/eonedmall/home/HomeRvAdapter.java
@@ -0,0 +1,119 @@
+package com.eoned.eonedmalll.eonedmall.home;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/2/25.
+ */
+
+public class HomeRvAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    /**
+     * 头布局类型
+     * */
+    private final int HEADER0 = 0;
+    private final int HEADER1 = 1;
+    private final int HEADER2 = 2;
+    private final int HEADER3 = 3;
+    private final int HEADER4 = 4;
+    private final int HEADER5 = 5;
+    private final int HEADER6 = 6;
+    private final int NORMAL = 100;
+
+    /**
+     * 头布局view
+     * */
+    private View headView0;
+    private View headView1;
+    private View headView2;
+    private View headView3;
+    private View headView4;
+    private View headView5;
+    private View headView6;
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType==HEADER0){
+            return new TopBannerHolder(headView0);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+
+        switch (viewType){
+            case HEADER0:
+
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+
+
+    /**
+     * 根据顺序设定不同的item类型
+     * */
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 && headView0 != null) {
+            return HEADER0;
+        } else if (position == 1 && headView1 != null) {
+            return HEADER1;
+        } else if (position == 2 && headView2 != null) {
+            return HEADER2;
+        } else if (position == 3 && headView3 != null) {
+            return HEADER3;
+        } else if (position == 4 && headView4 != null) {
+            return HEADER4;
+        } else if (position == 5 && headView5 != null) {
+            return HEADER5;
+        } else if (position == 6 && headView6 != null) {
+            return HEADER6;
+        } else {
+            return NORMAL;
+        }
+    }
+
+    class TopBannerHolder extends RecyclerView.ViewHolder{
+
+        public TopBannerHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public void setHeadView0(View headView0) {
+        this.headView0 = headView0;
+    }
+
+    public void setHeadView1(View headView1) {
+        this.headView1 = headView1;
+    }
+
+    public void setHeadView2(View headView2) {
+        this.headView2 = headView2;
+    }
+
+    public void setHeadView3(View headView3) {
+        this.headView3 = headView3;
+    }
+
+    public void setHeadView4(View headView4) {
+        this.headView4 = headView4;
+    }
+
+    public void setHeadView5(View headView5) {
+        this.headView5 = headView5;
+    }
+
+    public void setHeadView6(View headView6) {
+        this.headView6 = headView6;
+    }
+}
